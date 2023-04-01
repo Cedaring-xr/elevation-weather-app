75cd195797fe1d719fe909929114ff4f5523bae5
mattraydev <cedaring.xr@gmail.com>
2023-04-01 07:06:38 -0600

    unknown changes

--- elevation-weather/src/components/Header.tsx
+++ elevation-weather/src/components/Header.tsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 const Header = () => {
   return (
     <div className="header-container">
-      <h1 className='title main-title'>Cool title</h1>
+      <h1 className='title main-title'>Weather Search Query</h1>
       <Navbar />
     </div>
   );
