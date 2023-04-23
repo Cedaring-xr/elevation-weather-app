817b80d1056871f64f0091e53aca1c5b5ea61136
mattraydev <cedaring.xr@gmail.com>
2023-04-22 20:01:36 -0600

    added styling, assets, and structure

--- elevation-weather/src/components/Forcast.tsx
+++ elevation-weather/src/components/Forcast.tsx
@@ -1,29 +1,39 @@
 import React from 'react'
+import { TiWeatherShower } from 'react-icons/ti'
 
 const Forcast = ({title}: {title: string}) => {
     return (
         <div className='forcast-container'>
-            <h4>{title}</h4>
-            <hr />
-            <div>
-                <p>time</p>
-                <span>icon</span>
-                <p>55 degrees</p>
+            <div className='forcast-header'>
+                <h4>{title}</h4>
+                <hr />
             </div>
-            <div>
-                <p>time</p>
-                <span>icon</span>
-                <p>55 degrees</p>
-            </div>
-            <div>
-                <p>time</p>
-                <span>icon</span>
-                <p>55 degrees</p>
-            </div>
-            <div>
-                <p>time</p>
-                <span>icon</span>
-                <p>55 degrees</p>
+            <div className='forcast-data'>
+                <div className='forcast-block'>
+                    <p>time</p>
+                    <span><TiWeatherShower className='show-weather-icon'/></span>
+                    <p>55 degrees</p>
+                </div>
+                <div className='forcast-block'>
+                    <p>time</p>
+                    <span><TiWeatherShower className='show-weather-icon'/></span>
+                    <p>55 degrees</p>
+                </div>
+                <div className='forcast-block'>
+                    <p>time</p>
+                    <span><TiWeatherShower className='show-weather-icon'/></span>
+                    <p>55 degrees</p>
+                </div>
+                <div className='forcast-block'>
+                    <p>time</p>
+                    <span><TiWeatherShower className='show-weather-icon'/></span>
+                    <p>55 degrees</p>
+                </div>
+                <div className='forcast-block'>
+                    <p>time</p>
+                    <span><TiWeatherShower className='show-weather-icon'/></span>
+                    <p>55 degrees</p>
+                </div>
             </div>
         </div>
     )
--- elevation-weather/src/components/Header.tsx
+++ elevation-weather/src/components/Header.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import Navbar from './Navbar';
+import React from 'react'
+import Navbar from './Navbar'
 
 const Header = () => {
-  return (
-    <div className="header-container">
-      <h1 className='title main-title'>Weather Search Query</h1>
-      <Navbar />
-    </div>
-  );
+	return (
+		<div className="header-container">
+			<img className="header-gif" src="/assets/images/headerBackground.gif" />
+			<div className="header-elements">
+				<h1 className="title main-title">Elevation Weather</h1>
+				<h4>realtime weather search based on location or elevation</h4>
+				<Navbar />
+			</div>
+		</div>
+	)
 }
 
-export default Header;
+export default Header
