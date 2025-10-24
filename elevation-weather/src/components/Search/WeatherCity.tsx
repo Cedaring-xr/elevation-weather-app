3e2733b97d6f783fc9e4fe68976011c9869f1a54
cedaring-xr <cedaring.xr@gmail.com>
2025-10-24 11:21:53 -0600

    some changes to how weather is displayed

--- elevation-weather/src/components/Search/WeatherCity.tsx
+++ elevation-weather/src/components/Search/WeatherCity.tsx
@@ -10,32 +10,32 @@ type Props = {
 export default function WeatherCity({ city }: Props) {
 	return (
 		<div>
-			{/* <div className="flex flex-row justify-between items-center text-white w-2/3 md:w-1/2 mx-auto">
-				<span className="text-2xl">{city.name}</span>
-				<img src={iconsUrlFromCode(city.icon)} alt="" />
-				<p className="flex flex-col text-3xl">{city.temp.toFixed()}&deg;</p>
+			<div className="flex flex-row justify-between items-center text-white w-2/3 md:w-1/2 mx-auto">
+				<span className="text-2xl">{city.current.temp}</span>
+				<img src={iconsUrlFromCode(city.current.weather[0].icon)} alt="" />
+				<p className="flex flex-col text-3xl">{city.current.temp.toFixed()}&deg;</p>
 			</div>
 			<div className="flex flex-row items-center justify-center space-x-2 text-white py-1">
 				<p className="text-sm flex">
 					<BsDropletHalf className="text-sm flex" />
-					humidity: {city.humidity}%
+					humidity: {city.current.humidity}%
 				</p>
 				<span className="text-xl text-neutral-100 font-light mb-1">|</span>
 				<p className="text-sm flex">
 					<BsWind className="text-xl mr-1" />
-					wind: {city.speed.toFixed()} km/h
+					wind: {city.current.wind_speed.toFixed()} km/h
 				</p>
 				<span className="text-xl text-neutral-100 font-light mb-1">|</span>
 				<p className="text-sm flex">
 					<BsThermometerSnow className="text-xl mr-1" />
-					Low: {city.temp_min.toFixed()}&deg;
+					Low: {city.daily[0].temp.min.toFixed()}&deg;
 				</p>
 				<span className="text-xl text-neutral-100 font-light mb-1">|</span>
 				<p className="text-sm flex">
 					<BsThermometerSun className="text-xl mr-1" />
-					High: {city.temp_max.toFixed()}&deg;
+					High: {city.daily[0].temp.max.toFixed()}&deg;
 				</p>
-			</div> */}
+			</div>
 		</div>
 	)
 }
