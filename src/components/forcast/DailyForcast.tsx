0cbf893dd0dffc94b642235865579e4c9b795770
mattraydev <cedaring.xr@gmail.com>
2025-11-18 15:16:43 -0700

    fixed alignment display of daily and hourly

--- src/components/forcast/DailyForcast.tsx
+++ src/components/forcast/DailyForcast.tsx
@@ -31,18 +31,16 @@ const DailyForcast: React.FC<any> = ({ title, items, timezone }) => {
 				<h4 className="text-xl">{title}</h4>
 				<hr className="my-1" />
 			</div>
-			<div className="flex items-center justify-between">
+			<div className="flex items-start justify-between">
 				{items
 					? items.map((item: any, index: number) => (
-							<div key={item.dt}>
-								<div className="flex flex-col items-center justify-center">
-									<p className="font-medium">
-										{index === 0 ? 'Today' : formatToTimezone(item.dt, timezone, 'ccc')}
-									</p>
-									<img src={iconsUrlFromCode(item.weather[0].icon)} alt="icon" />
-									<p>{item.temp.day.toFixed()}&deg;</p>
-								</div>
-								<div className="">{item.weather[0].description}</div>
+							<div key={item.dt} className="flex flex-col items-center text-center w-20">
+								<p className="font-medium h-6">
+									{index === 0 ? 'Today' : formatToTimezone(item.dt, timezone, 'ccc')}
+								</p>
+								<img src={iconsUrlFromCode(item.weather[0].icon)} alt="icon" className="w-12 h-12" />
+								<p className="h-6">{item.temp.day.toFixed()}&deg;</p>
+								<p className="text-xs mt-1">{item.weather[0].description}</p>
 							</div>
 					  ))
 					: ''}
--- src/components/forcast/HourlyForcast.tsx
+++ src/components/forcast/HourlyForcast.tsx
@@ -10,18 +10,16 @@ const HourlyForcast: React.FC<any> = ({ title, items, timezone }) => {
 				<h4 className="text-xl">{title}</h4>
 				<hr className="my-1" />
 			</div>
-			<div className="flex items-center justify-between">
+			<div className="flex items-start justify-between">
 				{(items as ForcastHourlyProps)
 					? items.slice(0, 8).map((item: any, index: number) => (
-							<div key={item.dt}>
-								<div className="flex flex-col items-center justify-center">
-									<p className="font-medium">
-										{index === 0 ? 'Now' : formatToTimezone(item.dt, timezone, 'h a')}
-									</p>
-									<img src={iconsUrlFromCode(item.weather[0].icon)} alt="icon" />
-									<p>{item.temp.toFixed()}&deg;</p>
-								</div>
-								<div className="">{item.weather[0].description}</div>
+							<div key={item.dt} className="flex flex-col items-center text-center w-20">
+								<p className="font-medium h-6">
+									{index === 0 ? 'Now' : formatToTimezone(item.dt, timezone, 'h a')}
+								</p>
+								<img src={iconsUrlFromCode(item.weather[0].icon)} alt="icon" className="w-12 h-12" />
+								<p className="h-6">{item.temp.toFixed()}&deg;</p>
+								<p className="text-xs mt-1">{item.weather[0].description}</p>
 							</div>
 					  ))
 					: ''}
